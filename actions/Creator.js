b02b1b068df30c2c8c9cb2f29df48a42c2bd9103
关天汉 <14719752+guan-tianhan@user.noreply.gitee.com>
2025-03-30 22:17:49 +0800

    set APIs for create

--- actions/Creator.js
+++ actions/Creator.js
@@ -1,5 +1,12 @@
 module.exports = class Creator extends EventEmitter {
+
+    constructor (name, targetDir, promptModules = []) {
+        super()
+        this.name = name
+        this.targetDir = process.env.CYDON_CLI_TARGETDIR = targetDir
+    }
+
     create (cliOptions = {}, preset = null) {
-        
+
     }
 }
--- actions/test_create.js
+++ actions/test_create.js
@@ -1,5 +1,10 @@
 function create(projectName,options){
+    function getPromptModules(){
+        return []
+    }
 
+    const creator = new Creator(name, targetDir, getPromptModules())
+    creator.create(options)
 }
 
 module.exports = (...args)=>{
